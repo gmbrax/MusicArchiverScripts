1b57b1f9d47bd7d6c4232a7979f1af35ea5046a9
Gustavo Miranda <gustavohssmiranda@gmail.com>
2022-09-30 23:50:32 -0300

    Just Changed the version string and this commit also closed: #1

--- incTAR.py
+++ incTAR.py
@@ -9,7 +9,7 @@ from dataclasses import dataclass
 import glob
 import tarfile
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 loggerHandler = logging.handlers.RotatingFileHandler('./runlog.log')
 loggerFormatter = logging.Formatter(
